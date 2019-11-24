6dfa28832c0693fa87b5158071870001d0ee71ab
Dano Roost <dano.roost@gmail.com>
2019-11-24 18:39:47 +0100

    Revert change to update global model before loss calc.

--- dist-model-server/deliverables/network.pyx
+++ dist-model-server/deliverables/network.pyx
@@ -121,8 +121,6 @@ class AC_Network():
 
     def train(self, target_v, advantages, actions,  obs, num_agents_done):
         num_agents_done = np.min([1, num_agents_done])
-        
-        self.update_from_global_model()
 
         # Value loss
         with tf.GradientTape() as tape:
